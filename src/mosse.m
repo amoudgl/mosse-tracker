ba544dae2adc921281b946ee11dd9e1fc99e44df
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-08-31 10:54:56 +0530

    Average filters over training set

--- src/mosse.m
+++ src/mosse.m
@@ -9,7 +9,9 @@
 %     error('No image files found in the directory.');
 % end
 
-img_path = 'data/';
+clc; close all; clear;
+
+img_path = '../data/';
 D = dir([img_path, '*.jpg']);
 seq_len = length(D(not([D.isdir])));
 if exist([img_path num2str(1, '%05i.jpg')], 'file'),
@@ -42,7 +44,8 @@ gsize = size(im);
 g = gaussC(R,C, sigma, center);
 g = double2uint8(g);
 % imshow(g);
-h = divspec(g, im);
+H = ((fft2(g).*conj(fft2(im)))./(fft2(im).*conj(fft2(im))));
+h = double2uint8(ifft2(H));
 h = double(h);
 % imshow(h);
 % verify to get ground truth image from the obtained filter
@@ -57,14 +60,21 @@ for i = 1:N
 end
 
 for i = 1:N
-    hi = divspec(targets(:,:,i), train_set(:,:,i));
+    gi = targets(:,:,i);
+    fi = train_set(:,:,i);
+    Hi = ((fft2(gi).*conj(fft2(fi)))./(fft2(fi).*conj(fft2(fi))));
+    hi = double2uint8(ifft2(Hi));
     h = h + double(hi);
 end;
 
 h = h/N;
 h = double2uint8(h);
-figure; imshow(h);
+% figure; imshow(h);
 % verify to get ground truth image from the obtained filter
-% figure;
-% fig = imshow(im);
-% set(fig, 'AlphaData', multspec(h, im));
+figure;
+fig = imshow(im);
+set(fig, 'AlphaData', multspec(h, im));
+
+
+
+
