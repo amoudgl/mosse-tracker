12be65d05a867731516405e437ba94e18f348e0f
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-09-04 11:39:04 +0530

    Add preprocessing

--- src/preprocess.m
+++ src/preprocess.m
@@ -1,1 +1,8 @@
 function img = preprocess(img)
+[r,c] = size(img);
+win = window2(r,c,@hann);
+eps = 1e-5;
+img = log(double(img)+1);
+img = (img-mean(img(:)))/(std(img(:))+eps);
+img = img.*win;
+end
